87d6f58a977d5e31df885436637805ffcea4fac2
StephenAdedayo <alustephenadedayo@gmail.com>
2025-10-25 21:55:23 +0100

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const navigate = useNavigate()
+
+  return (
+    <nav className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>
+      <img onClick={() => navigate("/")} src={assets.logo} alt="logo"  className='w-32 cursor-pointer sm:w-44'/>
+      <button onClick={() => navigate('/admin')} className='flex group items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Login
+        <img src={assets.arrow} alt="arrow" className='w-3 group-hover:-rotate-90 transition-transform duration-500 ease-in cursor-pointer'/>
+      </button>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Bloglist from '../components/Bloglist'
+import NewsLetter from '../components/NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Bloglist />
+      <NewsLetter />
+    </>
+  )
+}
+
+export default Home
+Home
